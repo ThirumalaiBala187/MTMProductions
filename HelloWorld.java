bf5324bf9397339f7c099d211a9ada757f128ed9
yuvon-zstk383 <yuvon-zstk383@yuvon-zstk383.tsi.zohocorpin.com>
2025-02-11 10:56:35 +0530

    first

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld{
-  public static void main(STring args[]){
-    System.out,println("Hello World MTM");
+  public static void main(String args[]){
+    System.out.println("Hello World Zignite");
   }
 }
