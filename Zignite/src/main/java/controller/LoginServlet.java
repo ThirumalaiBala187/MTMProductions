a4a0ac2828acc210b4e51d5800600fc91e51b6be
yuvon-zstk383 <yuvon-zstk383@yuvon-zstk383.tsi.zohocorpin.com>
2025-02-12 10:40:54 +0530

    feb 12 10:40

--- Zignite/src/main/java/controller/LoginServlet.java
+++ Zignite/src/main/java/controller/LoginServlet.java
@@ -5,12 +5,16 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.*;
+
 
 public class LoginServlet extends HttpServlet {
 
@@ -27,16 +31,21 @@ public class LoginServlet extends HttpServlet {
         boolean isValidUser = validateUser(email, password);
         JSONObject jsonResponse = new JSONObject();
         if (isValidUser) {
+        	
             HttpSession session = request.getSession(true); 
             session.setAttribute("user", email);
+            JSONObject details=userDetails(email);
+            session.setAttribute("details", details);
             String sessionId = session.getId();
             Cookie sessionCookie = new Cookie("JSESSIONID", sessionId);
+            Cookie sessionCookie1 = new Cookie("DETAILS",details.toString());
             sessionCookie.setPath("/"); 
             sessionCookie.setMaxAge(60 * 60 * 24); 
             response.setContentType("application/json");
             response.addCookie(sessionCookie);
+            response.addCookie(sessionCookie1);
             PrintWriter out = response.getWriter();
-            out.print(jsonResponse.toString());
+            out.write(jsonResponse.toString());
 //            out.flush();
 //            response.sendRedirect("https://muthueshwaran-8389.zcodeusers.in/Zignite_Learnings/userHome/userHome.html#");
         } else {
@@ -46,9 +55,9 @@ public class LoginServlet extends HttpServlet {
             out.flush();
         }
     }
-    private boolean validateUser(String email, String password){
+    private boolean validateUser(String email, String password) {
     	try(Connection cn=Database.getConnection()){
-    		String sql="select User_Id, from Users where Name=? and Password=?";
+    		String sql="select User_Id,Role_Id from Users where Email=? and Password=?";
     		PreparedStatement st=cn.prepareStatement(sql);
     		st.setString(1, email);
     		st.setString(2, password);
@@ -61,6 +70,27 @@ public class LoginServlet extends HttpServlet {
     		}
     }
 }
+    private JSONObject userDetails(String email) {
+    	try(Connection cn=Database.getConnection()){
+    		JSONArray jsonArray=new JSONArray();
+    		String sql="select c.Course_Name,usp.XP,usp.StreakCount,usp.Levels_Completed from Users us inner join User_Progress usp on us.User_Id=usp.User_Id inner join Course c on c.Course_Id=usp.Course_Id where us.Email=?";
+    		PreparedStatement st=cn.prepareStatement(sql);
+    		st.setString(1, email);
+    		ResultSet rs=st.executeQuery();
+    		while(rs.next()) {
+    		JSONObject newObj=new JSONObject();
+    			newObj.put("course_name",rs.getString(1));
+    			newObj.put("xp",rs.getString(2));
+    			newObj.put("streakcount",rs.getString(3));
+    			newObj.put("levels_completed",rs.getString(4));
+    			jsonArray.put(newObj);
+    		}
+    		JSONObject userDetails=new JSONObject();
+    		userDetails.put("Courses", jsonArray);
+    		return userDetails;
+    }
+}
+    
 
 }
 
